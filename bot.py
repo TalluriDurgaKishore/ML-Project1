3295c73760c1a3da0aa29b169cea1ea96e002eb5
TalluriDurgaKishore <72603262+TalluriDurgaKishore@users.noreply.github.com>
2020-10-19 18:44:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,16 @@ def bot():
     greeting(name)
     introduction()
     input_data()
+    print()
     print('Do you want to know about something else..','1.YES','2.NO',sep='\n')
     choice = int(input('Enter your choice : '))   
     while choice == 1:
+        print()
         input_data()
+        print()
         print('Do you want to know about something else..','1.YES','2.NO',sep='\n')
         choice = int(input('Enter your choice : '))   
     if choice == 2:
+        print()
         print('Hope this is useful','Thankyou for coming here ..🤗🤗',sep='\n') 
 bot()
