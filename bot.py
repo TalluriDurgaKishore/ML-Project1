0ded70b1606f97d312a31cd4110e3613c97beffa
Tanuja303 <70839434+Tanuja303@users.noreply.github.com>
2020-10-19 17:39:44 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+from introduction import introduction,greeting
+from data import input_data
+
+def bot():
+    name = input('Enter your name here : ')
+    greeting(name)
+    introduction()
+    input_data()
+    print('Do you want to know about something else..','1.YES','2.NO',sep='\n')
+    choice = int(input('Enter your choice : '))   
+    while choice == 1:
+        input_data()
+        print('Do you want to know about something else..','1.YES','2.NO',sep='\n')
+        choice = int(input('Enter your choice : '))   
+    if choice == 2:
+        print('Hope this is useful','Thankyou for coming here ..🤗🤗',sep='\n') 
+bot()
